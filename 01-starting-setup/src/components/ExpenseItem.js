b840cc03c394c8c337ae7fb759b37935e239eb46
Benefrancis do Nascimento <benefrancis@gmail.com>
2022-03-26 19:26:46 -0300

    Atualizar o valor do elemento na tela depois da renderização inicial

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import ExpenseDate from './ExpenseDate';
 import Card from './Card';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
+    //Atualizar o valor do elemento na tela depois da renderização inicial
+    const [title, setTitle] = useState(props.title)
+
+    //Ouvindo o evento click
+    const clickHandler = () => {
+        //Atualizando
+        setTitle(title + ' Updated!')
+        console.log(title)
+    }
+
     return (
         <Card className='expense-item'>
             <ExpenseDate date={props.date} />
             <div className='expense-item__description'>
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
-                <button onClick={() => { console.log(props.title) }}>EVENTO</button>
+                <button onClick={clickHandler}>EVENTO</button>
             </div>
         </Card>
     );
