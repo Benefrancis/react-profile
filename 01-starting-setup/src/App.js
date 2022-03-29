90a4e61b756b30a191b1e62cab1aed8ea583b36b
Benefrancis do Nascimento <benefrancis@gmail.com>
2022-03-29 19:36:51 -0300

    Adicionando two way bind

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
-import Expenses from './components/Expense/Expenses';
-import ExpenseForm from './components/NewExpense/ExpenseForm';
+import NewExpense from './components/NewExpense/NewExpense';
+import Expenses from './components/Expenses/Expenses';
+
 const App = () => {
   const expenses = [
     {
@@ -25,10 +26,21 @@ const App = () => {
     },
   ];
 
+  const addExpenseHandler = expense => {
+    console.log('In App.js');
+    console.log(expense);
+  };
+
+  // return React.createElement(
+  //   'div',
+  //   {},
+  //   React.createElement('h2', {}, "Let's get started!"),
+  //   React.createElement(Expenses, { items: expenses })
+  // );
 
   return (
     <div>
-      <ExpenseForm />
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
     </div>
   );
